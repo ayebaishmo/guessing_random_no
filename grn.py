7470749a7e22d5b7fb1128e64a4bd63ffc1461c4
ayebaishmo <ishmo255super@gmail.com>
2024-04-21 17:30:28 +0300

    random number

--- grn.py
+++ grn.py
@@ -1,2 +1,25 @@
 import random
 
+# guess the number
+class ran_no:
+    def __init__(self, first):
+        self.first = first
+    
+    def r1_10(self):
+        self.first = random.randint(1, 5)
+        return self.first
+    
+    def __repr__(self):
+        return str(self.first)
+    
+    def your_inp(self, k):
+        self.k =k
+        if self.first == self.k:
+            return f"{self.k} and the right number {self.first} You guessed right"
+        else:
+            return f"{self.k} and the right number {self.first} You didn't get it right"
+    
+if __name__ == '__main__':
+    ran_no1 = ran_no(1)
+    print(ran_no1.r1_10())
+    print(ran_no1.your_inp(5))
